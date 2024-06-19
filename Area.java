bdc7709fa252b4d40c4f5483508d2a90b944cc5a
ANANTH SURAMPUDI 123 <surampudi.ananth123@gmail.com>
2024-06-19 06:28:46 -0500

    Shiva

---
+++ Area.java
@@ -0,0 +1,16 @@
+class Area{
+public static void main(String args[])
+{
+ 
+ int length = 2;
+ int breadth = 3 ;
+ int radius = 4;
+ double pie = 3.14;
+ int R_perimeter = 2*(length + breadth);
+ int R_area1 = (length * breadth);
+ double C_circumference = (double)( 2 * pie * radius);
+ double C_area2 = (double)(pie * radius * radius);
+ System.out.println("Rectangle: " + R_area1);
+ System.out.println("Circle: " + C_area2);
+}
+}
